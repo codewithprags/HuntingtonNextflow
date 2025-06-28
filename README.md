9f6b8c06cfdf54889823aea1129c168a8981922e
nguyenntkn <128745593+nguyenntkn@users.noreply.github.com>
2025-06-28 19:34:31 -0300

    Some comments in GSEA section

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # HuntingtonNextflow
+
+
+
+Sample groups are shown in SraRunTable.csv and GSE270472_HD_KO_NSC.xlsx:
+- Control (IC1):                    nH105IC1, nH105IC2, nH105IC3, nH105IC4
+- Huntington's Disease (HD/ND4222): nH222HD1, nH222HD2, nH222HD3, nH222HD4
+- HTT knock out (HTT-KO):           nH37KO1, nH37KO2, nH37KO3
+
+Resources: 
+- nf-core/fetchngs: https://nf-co.re/fetchngs/1.12.0/
+- nf-core/rnaseq: https://nf-co.re/rnaseq/3.19.0
+- nf-core/differentialabundance: https://nf-co.re/differentialabundance/1.5.0
--- Script/draft.sh
+++ Script/draft.sh
@@ -48,20 +48,7 @@ source ~/.bashrc
 ## hisat2, in this pipeline, doesnt actually quantify gene expression --> cant get deseq2 similarity plot. we used it bc star align was our of commission
 # DESeq2 = tool to quantify gene expression ; PC plot gives profile 
 
-./nextflow run nf-core/rnaseq \
---input ~/HuntingtonNextflow/Data/fetchngs/samplesheet/samplesheet.csv \
---outdir ~/HuntingtonNextflow/Data/alignment_run \
---fasta ["path to Human HTT refernce geneome,.fa.gz file type"] \
---gtf ["path to Human HTT refernce geneome,.gtf.gz file type"] \
---salmon_index "/HuntingtonNextflow/Data/index_run/genome/index/salmon" \
---trimmer fastp \
---aligner hisat2 \
---pseudo_aligner salmon \
---extra_salmon_quant_args "--gcBias --seqBias" \
---deseq2_vst true \
--w ~/HuntingtonNextflow/Data/alignment_run \
--profile docker
-# // OR //
+# Option 1: STAR and Salmon
 ./nextflow run nf-core/rnaseq \
 --input ~/HuntingtonNextflow/Data/fetchngs/samplesheet/samplesheet.csv \
 --outdir ~/HuntingtonNextflow/Data/alignment_run \
@@ -76,13 +63,62 @@ source ~/.bashrc
 -w ~/HuntingtonNextflow/Data/alignment_run \
 -profile docker
 
+# // OR //
+# Option 2: HISAT2 + Salmon
+./nextflow run nf-core/rnaseq \
+--input ~/HuntingtonNextflow/Data/fetchngs/samplesheet/samplesheet.csv \
+--outdir ~/HuntingtonNextflow/Data/alignment_run \
+--fasta ["path to Human HTT refernce geneome,.fa.gz file type"] \
+--gtf ["path to Human HTT refernce geneome,.gtf.gz file type"] \
+--salmon_index "/HuntingtonNextflow/Data/index_run/genome/index/salmon" \
+--trimmer fastp \
+--aligner hisat2 \
+--pseudo_aligner salmon \
+--extra_salmon_quant_args "--gcBias --seqBias" \
+--deseq2_vst true \
+-w ~/HuntingtonNextflow/Data/alignment_run \
+-profile docker
+
+
+
 # ================================ DEA ================================
+# Need to make:
+# - Count matrix
+# - Contrast file
+# - Transcript length matrix
+
+# DESeq2 + GSEA, gProfiler2, and Shiny App
 ./nextflow run nf-core/differentialabundance \
---input [file_path] \
---contrasts [file_path] \
---matrix [file_path] \
---transcript_length_matrix [file_path] \
---gtf [file_path] \
+--input ["file_path to meta data .tsv file"] \
+--contrasts ["file_path to contrast .tsv or .csv? file prodicing instructions on which group comparisons to perform"] \
+--matrix ["file_path to raw count matrix"] \
+--transcript_length_matrix ["file_path to transcript length matrix"] \
+--gtf ["file_path to gene annotation file (GRCh38.p13 files)"] \
+--filtering_min_proportion 0.3 \        # Filter out genes that are not expressed in at least 30% of samples => prevent noise from genes that are rarely expressed
+--filtering_grouping_var condition \    # Contrast variable (condition, or interaction, ..., whichever we specified in the contrast file??)
+--deseq2_cores 4 \
+--gsea_run true \
+--gsea_permute gene_set \               # Use gene set permutation since we have less than 7 samples per group (we only have 3-4)
+--gene_sets_files ["two file paths"]    # Gene symbol files? From MSigDB??
+--gprofiler2_run true \
+--gprofiler2_organism ["organism"] \    # Specify that we're using human genes 
+--gprofiler2_sources ["string"] \       # Select specific gene set database (GO, KEGG, REAC...)
+--gprofiler2_correction_method gSCS \   # Default for gProfiler, for small gene sets
+--shinyngs_build_app true \
+--max_cpus 8 --max_memory 8.GB \
+--outdir ~/scratch/lab_3/dge_analysis_filtered \
+-w ~/scratch/work/lab_3/dge_analysis \
+-profile [what profiles do we use here] # Which package + environmental manager to run the analysis in. We used Docker in the lab.
+
+
+# Additional
+# Basic DESeq2 + filtering
+./nextflow run nf-core/differentialabundance \
+--input ["file_path"] \
+--contrasts ["file_path"] \
+--matrix ["file_path"] \
+--transcript_length_matrix ["file_path"] \
+--gtf ["file_path"] \
 --deseq2_cores 4 \
 --filtering_min_proportion 0.3 \
 --filtering_grouping_var condition \
@@ -91,7 +127,7 @@ source ~/.bashrc
 -w ~/scratch/work/lab_3/dge_analysis \
 -profile [what profiles do we use here]
 
-
+# DESeq2 + GSEA
 ./nextflow run nf-core/differentialabundance \
 --input [file_path] \
 --contrasts [file_path] \
@@ -109,25 +145,3 @@ source ~/.bashrc
 -w ~/scratch/work/lab_3/dge_analysis \
 -profile [what profiles do we use here]
 
-
-./nextflow run nf-core/differentialabundance \
---input [file_path] \
---contrasts [file_path] \
---matrix [file_path] \
---transcript_length_matrix [file_path] \
---gtf [file_path] \
---filtering_min_proportion 0.3 \
---filtering_grouping_var condition \
---deseq2_cores 4 \
---gsea_run true \
---gsea_permute gene_set \
---gene_sets_files [two file paths]
---gprofiler2_run true \
---gprofiler2_organism [organism] \
---gprofiler2_sources [string] \
---gprofiler2_correction_method gSCS \
---shinyngs_build_app true \
---max_cpus 8 --max_memory 8.GB \
---outdir ~/scratch/lab_3/dge_analysis_filtered \
--w ~/scratch/work/lab_3/dge_analysis \
--profile [what profiles do we use here]
